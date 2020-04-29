4fb797f0f87b74931f59d2129f35daecbf513ac1
isidorostsa <63551950+isidorostsa@users.noreply.github.com>
2020-04-29 16:28:34 +0300

    sxolia handcombinations

--- handcombinations.cpp
+++ handcombinations.cpp
@@ -2,45 +2,17 @@
 // TODO: Implement here the method createAllCombinations of Hand
 
 void Hand::createAllCombinations(){ 
-    for(int startingIndex = 0; startingIndex < currentNumberOfCards; startingIndex ++){ 
-        Combination SINGLE = Combination();
+    for(int startingIndex = 0; startingIndex < currentNumberOfCards; startingIndex ++){ // loop over the hand
+        Combination SINGLE = Combination(); // create a "SINGLE" combination object for each of the cards
         SINGLE.addCard(cards[startingIndex]);
-        allCombinations[allCombinationsSize ++] = SINGLE;  
-        for(int loopingIndex = startingIndex + 1; loopingIndex < currentNumberOfCards; loopingIndex ++){
+        allCombinations[allCombinationsSize ++] = SINGLE;  // save it in allCombinations array
+        for(int loopingIndex = startingIndex + 1; loopingIndex < currentNumberOfCards; loopingIndex ++){ // loop over the cards after it to check for doubles
             if(cards[startingIndex].getValue() == cards[loopingIndex].getValue()){
-                Combination PAIR = Combination();
+                Combination PAIR = Combination(); // if a double is found create "PAIR" object 
                 PAIR.addCard(cards[startingIndex]);
-                PAIR.addCard(cards[loopingIndex]);
-                allCombinations[allCombinationsSize++] = PAIR; 
+                PAIR.addCard(cards[loopingIndex]);  // add both cards
+                allCombinations[allCombinationsSize++] = PAIR; // save it in allCombinations array
             }
         }
     }
 }
-
-
-//    for(int i = 0; i < currentNumberOfCards; i++){
-//		for(int j = 0; j < currentNumberOfCards - 1 - i; j++){
-//			if(cards[j].getValue() > cards[j + 1].getValue()){
-//				Card temp = cards[j];
-//				cards[j] = cards[j + 1];
-//				cards[j] = temp; 
-//			}
-//		}
-//	}
-//	for(int i = 0; i < currentNumberOfCards; i++){
-//        Combination SINGLE = Combination();
-//        allCombinations[allCombinationsSize++] = SINGLE;
-//        SINGLE.addCard(tempHand[i]);
-//        for(int j = 0; j < currentNumberOfCards; j++){
-//            if(tempHand[j].canBeInCombination() == false) continue; 
-//            if(tempHand[i].getValue() == tempHand[j].getValue()){
-//                Combination PAIR = Combination();
-//                PAIR.addCard(cards[i]);
-//				PAIR.addCard(cards[j]);
-//                allCombinations[allCombinationsSize++] = PAIR;
-//            }
-//        }
-//        
-//    }
-//}
-
