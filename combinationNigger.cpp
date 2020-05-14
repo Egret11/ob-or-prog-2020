8ddb6833ca4ea2bdbf929418a477c928797dea88
isidorostsa <63551950+isidorostsa@users.noreply.github.com>
2020-05-14 14:40:25 +0300

    Update combinationNigger.cpp

--- combinationNigger.cpp
+++ combinationNigger.cpp
@@ -1,3 +1,14 @@
 #include "combination.h"
 
 // TODO: Implement here the methods of Combination and all derived classes
+
+Combination::Combination(){
+
+}
+
+Combination::Combination(Card** cards, CombinationType type, int numberOfCards){
+    this->cards = cards;
+    this->type = type;
+    this->numberOfCards = numberOfCards;
+}
+
