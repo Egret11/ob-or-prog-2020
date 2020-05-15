5d97de782001936b746de6a79ecefda23f1dc378
Egret11 <v.kapa147@gmail.com>
2020-05-15 03:24:55 +0300

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -15,6 +15,20 @@ bool Player::combinationCanBePlayed(Combination *current, Combination *last){
     return (current->getType() == last->getType() && current->getNumberOfCards() == last->getNumberOfCards() && current->getValue() > last->getValue());  
 }
 
+int Player::countBucketPoints(){
+    int points = 0;
+    for(int i = 0; i != bucketSize; i++){
+        for(int j = 0; j != bucketSize; j++){
+            if(bucket[i][j].getValue() == 5){points+=5;}
+            else if(bucket[i][j].getValue() == 10){points+=10;}
+            else if(bucket[i][j].getValue() == 13){points+=10;}
+            else if(bucket[i][j].getSuit() == PHOENIX){points-=25;}
+            else if(bucket[i][j].getSuit() == DRAGON){points+=25;}
+        }
+    }
+    return points;
+}
+
 // TODO: Implement here the methods combinationCanBePlayed and countBucketPoints of Player
 // Do not implement any other methods of Player as these are already implemented in file playercombinations.cpp
 
