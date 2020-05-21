c9b9d7c3b87c569e3363b87273f7602d23c09505
isidorostsa <63551950+isidorostsa@users.noreply.github.com>
2020-05-21 17:48:50 +0300

    Update combinationNigger.cpp

--- combinationNigger.cpp
+++ combinationNigger.cpp
@@ -16,3 +16,22 @@ Combination::~Combination(){
     delete[] cards;
 }
 
+Straight::Straight(Card** cards, int numberOfCards):Combination(cards, STRAIGHT, numberOfCards){}
+
+bool Straight::cardsHaveTheSameSuit(){
+  for( int i = 1; i < getNumberOfCards(); i ++){
+    if(cards[0]->getSuit() != cards[i]->getSuit())
+        return false;
+  }
+  return true;
+}
+
+StraightFlush::StraightFlush(Straight* combination){
+    cards = new Card*[combination->getNumberOfCards()];
+    for(int i = 0; i < combination->getNumberOfCards(); i ++){
+        cards[i] = combination->getCard(i);
+    }
+    value = combination->getCard(combination->getNumberOfCards() - 1)->getValue();
+    numberOfCards = combination->getNumberOfCards();
+    type = STRAIGHTFLUSH;
+}
