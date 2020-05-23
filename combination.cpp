3e7ab8d8a3545ed73d3aa9f03accf75c797ad1ee
isidorostsa <63551950+isidorostsa@users.noreply.github.com>
2020-05-23 22:29:17 +0300

    telliko merge

--- combination.cpp
+++ combination.cpp
@@ -2,88 +2,153 @@
 
 // TODO: Implement here the methods of Combination and all derived classes
 
-Combination::Combination(){
+Combination::Combination(){} // an empty constructor
 
-}
 
-Combination::Combination(Card** cards, CombinationType type, int numberOfCards){
+Combination::Combination(Card** cards, CombinationType type, int numberOfCards){ // set stuff
     this->cards = cards;
     this->type = type;
     this->numberOfCards = numberOfCards;
 }
 
-Combination::~Combination(){
+
+Combination::~Combination(){ // delete the Card*[] 
     delete[] cards;
 }
 
-Single::Single(Card* card):Combination(cards, SINGLE, 1){
-  cards = new Card[1];
-  cards[0] = card;
+
+bool Combination::containsCard(Card* card)
+{
+    for (int i = 0; i < numberOfCards; i++)
+    {
+        if (cards[i]->equals(card)) return true; // card is contained in comb
+    }
+    return false; // card not contained in comb
 }
 
 
+CombinationType Combination::getType()
+{
+    return type;
+}
 
-Pair::Pair(Card* card1, Card* card2):Combination(cards, PAIR, 2){
-  cards = new Card[2];
+
+bool Combination::hasType(CombinationType type)
+{
+    return this->type == type;
+}
+
+
+int Combination::getValue()
+{
+    return value;
+}
+
+
+int Combination::getNumberOfCards()
+{
+    return numberOfCards;
+}
+
+
+bool Combination::equals(Combination* otherCombination)
+{
+    if (numberOfCards != otherCombination -> getNumberOfCards() || type != otherCombination-> getType() ) return false; // number of cards or type are different
+    for (int i = 0; i < numberOfCards; i++)
+    {
+        if (containsCard(otherCombination ->cards[i]) == false) return false; // card of othercomb is not in comb
+    }
+    return true; // combs match
+}
+
+
+Single::Single(Card* card){
+  cards = new Card*(card);
+  type = SINGLE ;
+  numberOfCards = 1;
+  value = card->getValue();
+}
+
+
+Pair::Pair(Card* card1, Card* card2){
+  cards = new Card*[2];
   cards[0] = card1;
   cards[1] = card2;
+
+  type = PAIR;
+  numberOfCards = 2;
+  value = card1->getValue();
 }
 
 
-ThreeOfAKind::ThreeOfAKind(Card* card1, Card* card2, Card* card3):Combination(cards, THREEOFAKIND, 3){
-  cards = new Card[3];
+ThreeOfAKind::ThreeOfAKind(Card* card1, Card* card2, Card* card3){
+  cards = new Card*[3];
   cards[0] = card1;
   cards[1] = card2;
   cards[2] = card3;
-}
 
+  type = THREEOFAKIND;
+  numberOfCards = 3;
+  value = card1->getValue();
+}
 
 
 Stairs::Stairs(Card** cards, int numberOfCards):Combination(cards, STAIRS, numberOfCards){
+    value = cards[numberOfCards-1]->getValue();
 }
 
 
-FullHouse::FullHouse(ThreeOfAKind* combination1, Pair* combination2):Combination(cards,FULLHOUSE,5){
-  cards = new Card[5];
+FullHouse::FullHouse(ThreeOfAKind* combination1, Pair* combination2){
+  cards = new Card*[5];
 
-  cards[0] = combination1[0];
-  cards[1] = combination1[1];
-  cards[2] = combination1[2];
+  cards[0] = combination1->getCard(0);
+  cards[1] = combination1->getCard(1);
+  cards[2] = combination1->getCard(2);
 
-  cards[3] = combination2[0];
-  cards[4] = combination2[1];
+  cards[3] = combination2->getCard(0);
+  cards[4] = combination2->getCard(1);
+
+  type = FULLHOUSE;
+  numberOfCards = 5;
+  value = 10*(combination1->getValue()) + combination2->getValue();
 
 }
 
 
-//
 Straight::Straight(Card** cards, int numberOfCards):Combination(cards, STRAIGHT, numberOfCards){
+  value = cards[numberOfCards-1]->getValue();
 }
-
 
 
 bool Straight::cardsHaveTheSameSuit(){
-  for( int i = 0; i < getNumberOfCards() - 1 ; i++ ){
-    if(cards[i].getSuit() != cards[i+1].getSuit())
+  for( int i = 1; i < getNumberOfCards(); i++){
+    if(cards[0]->getSuit() != cards[i]->getSuit())
         return false;
   }
   return true;
 }
 
 
-
 FourOfAKind::FourOfAKind(Card* card1, Card* card2, Card* card3, Card* card4){
-  cards = new Card[4];
+  cards = new Card*[4];
   cards[0] = card1;
   cards[1] = card2;
   cards[2] = card3;
   cards[3] = card4;
 
-  Combination( cards, FOUROFAKIND, 4);
+  type = FOUROFAKIND;
+  numberOfCards = 4;
+  value = card1->getValue();
 }
 
 
-
 StraightFlush::StraightFlush(Straight* combination){
- Combination( combination, STRAIGHT, getNumberOfCards());
+  cards = new Card*[combination->getNumberOfCards()];
+  for(int i = 0; i < combination->getNumberOfCards(); i++){
+    cards[i] = combination->getCard(i);
+  }
+  
+  value = combination->getCard(combination->getNumberOfCards()-1)->getValue();
+  numberOfCards = combination->getNumberOfCards();
+  type = STRAIGHTFLUSH;
 }
--- player.cpp
+++ player.cpp
@@ -3,7 +3,7 @@
 
 int Player::countBucketPoints(){
     int points = 0;
-    for(int i = 0; i != bucketSize; i++){
+    for(int i = 0; i != bucketSize; i++){ // loop over the bucket to add the points of any point-having card
             if(bucket[i]->getValue() == 5){points+=5;}
             else if(bucket[i]->getValue() == 10){points+=10;}
             else if(bucket[i]->getValue() == 13){points+=10;}
@@ -15,24 +15,24 @@ int Player::countBucketPoints(){
 
 
 bool Player::combinationCanBePlayed(Combination *current, Combination *last){
-    if(last->getType() == FOUROFAKIND && current->getType() == FOUROFAKIND){
-    return current->getValue() > last->getValue();
+    if(last->getType() == FOUROFAKIND && current->getType() == FOUROFAKIND) return current->getValue() > last->getValue(); // FOAK on FOAK
+    
+    else if( (current->getType() == STRAIGHTFLUSH) && (last->getType() == FOUROFAKIND) ) return true; // SF ON FOAK
+    
+    else if(current->getType() == STRAIGHTFLUSH && last->getType() == STRAIGHTFLUSH){ // SF on SF
+        
+        if(current->getNumberOfCards() > last->getNumberOfCards()) return true; // current has more cards 
+        
+        else if( (current->getNumberOfCards() == last->getNumberOfCards()) 
+                && (current->getValue() > last->getValue()) ) return true; // equal length but current is higher
+
+        else return false; // then current is shorter
     }
-    else if(current->getType() == STRAIGHTFLUSH && last->getType() == FOUROFAKIND){
-        return true;
-    }
-    else if(current->getType() == STRAIGHTFLUSH && last->getType() == STRAIGHTFLUSH){
-        if(current->getNumberOfCards() >= last->getNumberOfCards()) {return true;}
-        else if(current->getValue() > last->getValue() && current->getNumberOfCards() == last->getNumberOfCards()){return true;} 
-    }
-    else if((current->getType() == STRAIGHTFLUSH || current->getType() == FOUROFAKIND) && (last->getType() != FOUROFAKIND && last->getType() != STRAIGHTFLUSH) ){
-        return true;
-    } 
-    else
-    return (current->getType() == last->getType() && current->getNumberOfCards() == last->getNumberOfCards() && current->getValue() > last->getValue());  
+    
+    else if( current->getType() == STRAIGHTFLUSH || current->getType() == FOUROFAKIND
+             && last->getType() != FOUROFAKIND && last->getType() != STRAIGHTFLUSH ) return true; // bomb on NOT bomb
+    
+    return (current->getType() == last->getType())                          // same type 
+            && (current->getNumberOfCards() == last->getNumberOfCards())    // equal length (for stairs and straights)
+            && (current->getValue() > last->getValue());                    // current is higher
 }
-
-// TODO: Implement here the methods combinationCanBePlayed and countBucketPoints of Player
-// Do not implement any other methods of Player as these are already implemented in file playercombinations.cpp
-
-
